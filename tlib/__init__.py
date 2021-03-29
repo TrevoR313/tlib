d07a4e8f15006f7d8a72a630711e211d2da66940
Trevor <trevor@313.su>
2021-03-29 03:33:36 -0400

    ignore case, remove useless funcs

--- tlib/__init__.py
+++ tlib/__init__.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from subprocess import run
 import re
 
+
 def download(url: str, fname: str):
     try:
         with open(fname, "ab") as f, tqdm(
@@ -15,17 +16,18 @@ def download(url: str, fname: str):
                 headers["Range"] = f"bytes={pos}-"
             resp = get(url, headers=headers, stream=True)
             total_size = int(resp.headers.get("content-length", 0))
-            bar.desc=fname
-            bar.iterable=resp.iter_content(chunk_size=1024)
-            bar.total=total_size
-            bar.unit="iB"
-            bar.unit_scale=True
-            bar.unit_divisor=1024
+            bar.desc = fname
+            bar.iterable = resp.iter_content(chunk_size=1024)
+            bar.total = total_size
+            bar.unit = "iB"
+            bar.unit_scale = True
+            bar.unit_divisor = 1024
             for data in resp.iter_content(chunk_size=1024):
                 size = f.write(data)
                 bar.update(size)
     except KeyboardInterrupt:
         quit()
+
 
 def gen(num, sep):
     i, o = 0, ""
@@ -86,7 +88,7 @@ def isVideo(_file: str) -> bool:
     :returns: A bool
     :rtype: bool
     """
-    if path.splitext(_file)[-1] in ['.m4v', '.mp4', '.mov', '.wmv', '.avi', '.mpg', '.mpeg', '.rmvb', '.rm', '.flv', '.asf', '.mkv', '.webm']:
+    if path.splitext(_file.lower())[-1] in ['.m4v', '.mp4', '.mov', '.wmv', '.avi', '.mpg', '.mpeg', '.rmvb', '.rm', '.flv', '.asf', '.mkv', '.webm']:
         return True
     else:
         return False
@@ -100,7 +102,7 @@ def isImage(_file: str) -> bool:
     :returns: A bool
     :rtype: bool
     """
-    if path.splitext(_file)[-1] in ['.png', '.jpg', '.jpeg', '.gif', '.webp']:
+    if path.splitext(_file.lower())[-1] in ['.png', '.jpg', '.jpeg', '.gif', '.webp']:
         return True
     else:
         return False
@@ -114,28 +116,10 @@ def isAudio(_file: str) -> bool:
     :returns: A bool
     :rtype: bool
     """
-    if path.splitext(_file)[-1] in ['.mp3', '.aac', '.ogg', '.m4a', '.wma', '.flac', '.wav']:
+    if path.splitext(_file.lower())[-1] in ['.mp3', '.aac', '.ogg', '.m4a', '.wma', '.flac', '.wav']:
         return True
     else:
         return False
-
-
-def isDir(_directory: str) -> bool:
-    """
-    Checks if _directory is an actual directory and returns a corresponding bool.
-    :param _directory: Path the the directory.
-    :type _directory: str
-    :returns: A boolean that is True if _directory is a directory, returns False if it doesn't exist and/or it's not a directory.
-    :rtype: bool
-    """
-    try:
-        _dir = open(_directory, 'r')
-        _dir.close()
-    except IsADirectoryError:
-        return True
-    except FileNotFoundError:
-        return False
-    return False
 
 
 def parseCookieFile(cookiefile: str) -> dict:
@@ -149,6 +133,7 @@ def parseCookieFile(cookiefile: str) -> dict:
                 lineFields = line.strip().split('\t')
                 cookies[lineFields[-1 - 1]] = lineFields[-1]
     return cookies
+
 
 def parseHeaders(Headers: str) -> dict:
     """
@@ -167,3 +152,4 @@ def parseHeaders(Headers: str) -> dict:
     for i in list_parsed:
         parsed.update({i[0]: i[-1]})
     return parsed
+
